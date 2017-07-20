04d990062046498de7aab644178b87c512315d3a
jenniferjara <jennifer.jara@mambo.pe>
2017-07-20 12:22:26 -0500

    update font family titles

--- resources/views/common/navbar.blade.php
+++ resources/views/common/navbar.blade.php
@@ -1,15 +1,19 @@
 <header id="nav-bar">
 	<div class="container">
-		<nav class="nav-top">
-			<a href="{{ route('home.index') }}">
-				<span class="da_logo_da nav-logo"></span>
-			</a>
-			<a class="nav-perfil button-collapse">
-				<span class="da_ico_user nav-perfil-icon"></span>
-			</a>
-			<a class="nav-barra button-collapse">
-				<span class="material-icons">_menu</span>
-			</a>
-		</nav>
+		<div class="row">
+			<div class="col s12">
+				<nav class="nav-top">
+					<a href="{{ route('home.index') }}">
+						<span class="da_logo_da nav-logo"></span>
+					</a>
+					<a class="nav-perfil button-collapse">
+						<span class="da_ico_user nav-perfil-icon"></span>
+					</a>
+					<a class="nav-barra button-collapse">
+						<span class="material-icons">_menu</span>
+					</a>
+				</nav>
+			</div>
+		</div>
 	</div>
 </header>
